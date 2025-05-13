2788ea769faf1ec7774d537ae13d3534ebd0bf50
hrimasteval <sangelov42069@gmail.com>
2025-05-13 11:08:55 +0300

    Add project files.

---
+++ HelloApi/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok(new { Message = "Hello, world!" });
+        }
+    }
+}
