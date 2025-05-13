7e769f99a7118646b786649909fa46449895fbaf
RoskataBrat <ruslant.20b@gmail.com>
2025-05-13 13:44:10 +0300

    Add files via upload

---
+++ HelloApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HelloApi.Models;
+
+namespace HelloApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Calories { get; set; }
+    }
+}
---
+++ HelloApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Calories { get; set; }
+    }
+}
